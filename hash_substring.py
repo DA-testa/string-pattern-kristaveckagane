82121cf2a822a2723cd8f22c7f9327167ca73b33
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-04-16 19:30:53 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -12,10 +12,10 @@ def read_input():
     if "f" == ievade.lower():
         try:
             file = input("").strip()
-            with open("tests/" + file, mode="r") as f:
-                lines = f.readlines()
-                pattern = lines[0].rstrip()
-                text = lines[1].rstrip()
+            with open("tests/06.txt", mode="r") as f:
+              lines = f.readlines()
+              pattern = lines[0].rstrip()
+              text = lines[1].rstrip()
             return (pattern, text)
         except OSError as e:
             print(e)
