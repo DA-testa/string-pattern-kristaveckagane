d58d37b5c460b371002fb8b24d3d9e128d7ac995
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-04-16 14:40:28 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -1,17 +1,15 @@
-# python3
 B = 13
 Q = 256
+
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     ievade = input().strip()
     if "f" == ievade.lower():
         file = input("").strip()
         try:
-         file = open("./tests/" + file, mode = "r")
-         pattern = file.readline().rstrip()
-         text = file.readline().rstrip()
-         return (pattern, text)
+            file = open("./tests/" + file, mode="r")
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+            return (pattern, text)
         except OSError as e:
             print(e)
     if "i" == ievade.lower():
@@ -19,39 +17,39 @@ def read_input():
         text = input().rstrip()
         return (pattern, text)
 
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    #return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+ 
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     global B,Q
     r = 0
-    # this function should find the occurances using Rabin Karp alghoritm 
+
     intpattern = int(pattern)
-    intpattern_len=len(intpattern)
+    intpattern_len = len(intpattern)
     for i in range(intpattern_len):
-     r = (B*r + ord(pattern[i])) % Q
+        r = (B * r + ord(pattern[i])) % Q
     tlen = len(text)
-    plen=len(pattern)
+    plen = len(pattern)
     m = 1
-    for i in range(1,plen):
-     m = (multiple r * B) % Q
+    for i in range(1, plen):
+        m = (m * B) % Q
+    result = []
+    thash = 0
+    for i in range(plen):
+        thash = (B * thash + ord(text[i])) % Q
+    for s in range(tlen - plen + 1):
+        if thash == r:
+            if text[s:s + plen] == pattern:
+                result.append(s)
+        if s < tlen - plen:
+            thash = (thash - m * ord(text[s])) % Q
+            thash = (thash * B + ord(text[s + plen])) % Q
+            thash = (thash + Q) % Q
+    return result
 
-    # and return an iterable variable
-    return [0]
 
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
